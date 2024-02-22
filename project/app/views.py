1a2fb989eaf34960b726fa3eddb10af94fd7fbed
Azhan Khan <azhanborntowin22@gmail.com>
2024-02-22 20:59:41 +0530

    third

--- project/app/views.py
+++ project/app/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from .models import User
 
 # Create your views here.
 def home(request):
     return render(request,'home.html')
+
+
+def contact(request):
+    name = request.POST.get('name')
+    email = request.POST.get('email')
+    message = request.POST.get('message')
+    
+
+    User.objects.create(
+        Name = name,
+        Email = email,
+        Message = message
+    )
+    msg = "Send Successfully"
+    return render(request,'home.html',{"msg":msg})
