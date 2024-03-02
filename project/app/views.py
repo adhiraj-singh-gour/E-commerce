48ca11c9dca2db0261cb19a1e59c0475a71a6167
Adhiraj Singh <adhirajsingh3103.com>
2024-03-02 15:13:12 +0530

    admin

--- project/app/views.py
+++ project/app/views.py
@@ -9,6 +9,65 @@ def home(request):
     data2 = Products.objects.filter(Type="glasses")
     return render(request, "home.html", {"ts": data,"sh":data1,"sg":data2})
 
+def signup(request):
+    return render(request, "signup.html")
+    
+def signin(request):
+    return render(request, "signin.html")
+
+def usersignup(request):
+    if request.method == "POST":
+        username=request.POST.get('username')
+        email=request.POST.get('email')
+        password=request.POST.get('password')
+        cpassword=request.POST.get('cpassword')
+        user = Signup.objects.filter(email=email)
+
+        if user:
+            message = "User already exist"
+            return render(request, "signup.html", {"msg": message})
+        else:
+            if password == cpassword:
+                newuser = Signup.objects.create(
+                    username=username,
+                    email=email,
+                    password=password,
+                    Cpassword=cpassword
+                )
+                message = "User register Successfully"
+                return render(request, "signin.html", {"msg": message})
+            else:
+                message = "Password and Confirm Password Does not Match"
+                return render(request, "signup.html", {"msg": message})
+            
+
+
+def usersignin(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        # Checking the emailid with database
+        user = Signup.objects.filter(username=username)
+        if user:
+            data = Signup.objects.get(username=username)
+            if data.password == password:
+                username = data.username
+                email = data.email
+                password = data.password
+                user = {
+                    "username": username,
+                    "email": email,
+                    "password": password,
+                }
+                return render(request, "home.html", user)
+            else:
+                message = "Password does not match"
+                return render(request, "login.html", {"msg": message})
+        else:
+            message = "User does not exist"
+            return render(request, "register.html", {"msg": message})
+
 
 
 def contact(request):
@@ -43,11 +102,6 @@ def add_cart(request,pk):
     data = Products.objects.get(id=pk)
     return render(request,'productDetail.html', {"data": data})
 
-
-def adminpage(request):
-    return render(request,'admin.html')
-
-
 def pro_detail(request):
     if request.method == "POST":
         form = Product(request.POST, request.FILES)
@@ -60,3 +114,33 @@ def pro_detail(request):
 def show(request):
     data = Products.objects.all()
     return render(request, "show.html", {"data": data})
+
+
+
+
+# admin pannel
+def adminpage(request):
+    data = Products.objects.all()
+    return render(request,'admin.html',{'data':data})
+
+def viewuser(request):
+    user = Signup.objects.all()
+    return render(request,'viewuser.html',{'user':user})
+
+def update(request,pk):
+    data = Products.objects.get(id=pk)
+    return render(request,'add.html',{'data':data})
+
+def update_product(request):
+    if request.method == "POST":
+        form = Product(request.POST, request.FILES)
+        if form.is_valid():
+            form.save() 
+    form = Product()
+    return render(request, "update.html", {"form": form})
+
+def delete(request, pk):
+    data = Products.objects.get(id=pk)
+    data.delete()
+    data=Products.objects.all()
+    return render(request,'admin.html')
